e33b611cff55229747af9f0b969a66741b341d9a
Ibrahim Almeyda <almeydaibrahim@gmail.com>
2026-02-16 14:49:45 -0500

    All variables and strings labs completed

---
+++ variables_strings/sentence_maker.js
@@ -0,0 +1,23 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mystical cave";
+let adjective2 = "fiery";
+let noun2 = "marshamallows";
+
+// firs story
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "tiny";
+noun = "unicorn";
+verb = "excited";
+place = "enchanted forest";
+adjective2 = "sparkling";
+noun2 = "rainbow cookies";
+
+// second story
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log("Second story: " + secondStory);
---
+++ variables_strings/trivia_bot.js
@@ -0,0 +1,18 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "TriviaBot";
+const botLocation = "the internet";
+const favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+
+let codingFact = `He built the first version in only 10 days! 😅 Originally, it was called Mocha, then LiveScript, and finally renamed ${favoriteLanguage} — mainly for marketing reasons to ride the popularity of Java at the time.`;
+console.log(codingFact);
+codingFact = `Despite the name, ${favoriteLanguage} has almost nothing to do with Java. Java was created by James Gosling at Sun Microsystems. The similarity in names was mostly a branding strategy. Under the hood, they are completely different languages with different paradigms and runtimes.`;
+console.log(codingFact);
+codingFact = `Originally, ${favoriteLanguage} only ran in browsers. But in 2009, Ryan Dahl created Node.js, allowing JavaScript to run on servers.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);
